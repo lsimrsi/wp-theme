8d3d0093e544b385d0fa7ca5410e36ef30ad1191
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-01-05 15:58:16 -0600

    functions

---
+++ wp-content/themes/tumeric/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+if ( ! function_exists( 'myfirsttheme_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress  
+ * features.
+ *
+ * It is important to set up these functions before the init hook so
+ * that none of these features are lost.
+ */
+function myfirsttheme_setup() {  }
+endif;
