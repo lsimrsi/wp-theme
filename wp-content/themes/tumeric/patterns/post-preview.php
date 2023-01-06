f4842ab072e30867eb7992e1290974111c5db97a
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-01-06 16:07:10 -0600

    use pattern

---
+++ wp-content/themes/tumeric/patterns/post-preview.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Post Preview
+ * Slug: tumeric/post-preview
+ */
+?>
+<!-- wp:post-template {"align":"wide"} -->
+<!-- wp:group -->
+<div class="wp-block-group">
+  <!-- wp:post-featured-image {"isLink":true,"height":"200px"} /-->
+  <!-- wp:post-title {"isLink":true} /-->
+  <!-- wp:post-excerpt /-->
+  <!-- wp:post-date /-->
+</div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
